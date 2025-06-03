b40a1a50908892a25374d2f8b05697018b38520a
SHIWANG26 <shiwangr@gmail.com>
2025-06-04 00:09:45 +0530

    made some changes in auth

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,28 +1,28 @@
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
-import { ApiError } from '../utils/apiError.js';
-import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js"
 
-export const verifyJWT = asyncHandler(async (req, _, next) => {
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-
-    // Check if token is provided
-    if (!token) {
-        return next(new ApiError(401, "Unauthorized! No token provided."));
-    }
+export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-        // Check if user exists
-        if(!user){
-            throw new ApiError(401, "Unauthorized! User not found.")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "") 
+    
+        if(!token){
+            throw new ApiError(401, "Unauthorized request")
         }
-        req.user = user
-
-        //transfer control from middleware to user controller
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id)
+        .select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
         next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Unauthorized! Invalid token.")
+        throw new ApiError(401, error?.message || "Invalid Access Token")
     }
 })
